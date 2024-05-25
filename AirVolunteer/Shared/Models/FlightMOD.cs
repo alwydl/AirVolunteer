3fd12df7f695c9b9939bc4488d2c6536d2fc3db8
Alwy <alwydl@gmail.com>
2024-05-25 12:36:47 -0300

    2405251236

--- AirVolunteer/Shared/Models/FlightMOD.cs
+++ AirVolunteer/Shared/Models/FlightMOD.cs
@@ -1,7 +1,27 @@
-﻿namespace Shared
-{
-    public class Class1
-    {
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 
+namespace Shared.Models
+{
+    public class FlightMOD
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string OriginAirport { get; set; }
+        [Required]
+        public string DestinyAirport { get; set; }
+        [Required]
+        public string AirplaneModel { get; set; }
+        [Required]
+        public int PaxPositionsAvailable { get; set; }
+        [Required]
+        public int FullPaxMaxLoad { get; set; }
+        [Required]
+        public Guid PilotId { get; set; }
+        public virtual PilotMOD Pilot { get; set; }
+        [Required]
+        public DateTime Departure { get; set; }
+        public DateTime Arrival { get; set; }
     }
 }
